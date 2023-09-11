e639453a5a1b5078d97a31e8d6ff1716c28b2710
nesta.web <nesta.web.dev@gmail.com>
2023-09-12 00:35:16 +0200

    header

---
+++ README.md
@@ -0,0 +1,3 @@
+Créez l'Extérieur, Imaginez l'Intérieur.
+
+Extérieur Prêt, Intérieur À Créer.
--- index.php
+++ index.php
@@ -15,42 +15,42 @@ include_once findPath($root, "head.php")
 <body>
     <?php include_once findPath($root, "navbar.php") ?>
     <header>
-		<!-- <div class="header"></div> -->
-		<div class="containerHeader">
-			<div class="leftHeader">
-				<div class="title">
-					<h1></h1>
+		<div class="innerHeader">
+
+			<div class="containerHeader">
+				<div class="leftHeader">
+					<h1><span class="firstPart part"><span class="firstLetter" id="letter0">Créez</span> l'Extérieur,</span> <span class="secondPart part"><span class="firstLetter" id="letter1">Imaginez</span> l'Intérieur.</span></h1>
+				</div>
+				<div class="rightHeader">
+					<div class="decorationshape"></div>
+					<img data-id="2" src="<?= findPath($root, "header2.webp") ?>" alt="house contruction wood">
+					<!-- <img data-id="3" src="<?= findPath($root, "header2.webp") ?>" alt="house contruction wood"> -->
 				</div>
 			</div>
-			<div class="rightHeader">
-				<div class="decorationshape"></div>
-				<img data-id="2" src="<?= findPath($root, "header2.webp") ?>" alt="house contruction wood">
-				<!-- <img data-id="3" src="<?= findPath($root, "header2.webp") ?>" alt="house contruction wood"> -->
-			</div>
+			<form class="selectZone" method="post" action="./backend/test.php">
+				<div class="filtres">
+					<div class="filtre">
+						<input type="text" name="localisation" class="question" id="city" required autocomplete="off" />
+						<label for="city"><span>Où souhaitez-vous habiter ?</span></label>
+					</div>
+					<div class="filtre hidden">
+						<select class="minimal" name="type" required>
+							<option value="0" disabled selected>Quel type de bien ?</option>
+							<option value="1">Appartement</option>
+							<option value="2">Maison</option>
+							<option value="3">Terrain</option>
+						</select>
+					</div>
+					<div class="filtre hidden">
+						<input type="number" name="prix" class="price" id="price" required autocomplete="off" />
+						<label for="price"><span>Prix maximun</span></label>
+					</div>
+				</div>
+				<div class="search">
+					<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M686-450H160v-60h526L438-758l42-42 320 320-320 320-42-42 248-248Z"/></svg>
+				</div>
+			</form>
 		</div>
-		<form class="selectZone" method="post" action="./backend/test.php">
-			<div class="filtres">
-				<div class="filtre">
-					<input type="text" name="localisation" class="question" id="city" required autocomplete="off" />
-					<label for="city"><span>Où souhaitez-vous habiter ?</span></label>
-				</div>
-				<div class="filtre hidden">
-					<select class="minimal" name="type" required>
-						<option value="0" disabled selected>Quel type de bien ?</option>
-						<option value="1">Appartement</option>
-						<option value="2">Maison</option>
-						<option value="3">Terrain</option>
-					</select>
-				</div>
-				<div class="filtre hidden">
-					<input type="number" name="prix" class="price" id="price" required autocomplete="off" />
-					<label for="price"><span>Prix maximun</span></label>
-				</div>
-			</div>
-			<div class="search">
-				<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M686-450H160v-60h526L438-758l42-42 320 320-320 320-42-42 248-248Z"/></svg>
-			</div>
-		</form>
 	</header>
 	<main>
 		<div class="featuredHouses">
