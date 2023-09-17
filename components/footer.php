62f81206a9655c83e4b2dd20f4fe908088b5ad1d
nesta.web <nesta.web.dev@gmail.com>
2023-09-17 20:41:53 +0200

    footer

---
+++ components/footer.php
@@ -0,0 +1,63 @@
+<footer>
+    <div class="round"></div>
+    <div class="innerFooter">
+        <div class="topFooter">
+            <div class="leftTopFooter"><img src="../assets/logo.png" alt="logo IDEALKIT"></div>
+            <div class="rightTopFooter">
+                <p class="email">Email : john.doe@gmail.com</p>
+                <p class="phone">Téléphone : 02 40 03 20 20</p>
+            </div>
+        </div>
+        <div class="midFooter">
+            <div class="containerFooter">
+                <p class="title">A propos</p>
+                <p class="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            </div>
+            <div class="containerFooter">
+                <p class="title">Navigation</p>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">A propos</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="containerFooter">
+                <p class="title">Ressources</p>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">A propos</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="containerFooter">
+                <p class="title">Nos outils</p>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">A propos</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="containerFooter">
+                <p class="title">Nous contacter</p>
+                <ul>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">A propos</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="bottomFooter">
+            <div class="leftBottomFooter">
+                <p class="bottomDoc">Mentions légales</p>
+            </div>
+            <div class="midBottomFooter">
+                <p class="midInfo">© 2023 IDÉALKIT. Tous Droits Réservés.</p>
+            </div>
+            <div class="rightBottomFooter">
+                <div class="contactFooter">
+                    Contact
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
